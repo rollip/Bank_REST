f8f276a4406acc54ce850c8ac1e92eeff33ba84c
rollip <86199091+rollip@users.noreply.github.com>
2025-09-20 00:30:51 +0300

    feat: CORS config

--- src/main/java/com/example/bankcards/config/SecurityConfig.java
+++ src/main/java/com/example/bankcards/config/SecurityConfig.java
@@ -15,24 +15,33 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthFilter jwtAuthFilter;
+    @Value("#{'${application.allowed-origins}'.split(',')}")
+    private List<String> allowedOrigins;
 
-    @Value("${application.no-auth-endpoints}")
-    private String WHITELIST;
+    @Value("#{'${application.no-auth-endpoints}'.split(',')}")
+    private String[] whitelist;
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
             http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                     .authorizeHttpRequests(auth -> auth
-                            .requestMatchers(WHITELIST.split(",")).permitAll()
+                            .requestMatchers(whitelist).permitAll()
                             .anyRequest().authenticated()
                     )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
@@ -49,6 +58,18 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration generalConfig = new CorsConfiguration();
+        generalConfig.setAllowedOriginPatterns(allowedOrigins);
+        generalConfig.setAllowedMethods(List.of("OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"));
+        generalConfig.setAllowedHeaders(List.of("*"));
+        generalConfig.setAllowCredentials(false);
+        source.registerCorsConfiguration("/**", generalConfig);
+        return source;
     }
 
 }
